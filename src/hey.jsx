fd7e279fe49ea09edd346d7a4e0179f4bcfad0be
Alibaba <abdulmanafaliu414@gmail.com>
2024-06-18 06:26:36 +0000

    minor changes

---
+++ src/hey.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const hanldeNavigate = () => {
+    
+  }
+
+  return (
+    <>
+     <div className='rating-container'>
+      <img src="./images/icon-star.svg" alt="icon-star" />
+      <div className='rating-info'>
+      <h2>How did we do?</h2>
+      <p className='message' style={{marginTop: '0.5em', color: 'hsl(217, 12%, 63%)',}}>
+        Please let us know how we did with your support request.
+        All feedback is appreciated to help us improve our offering!
+      </p>
+      </div>
+      <div >
+        <div className='rating-btn'>
+            <button class="rate-btn" id="rateBtn" onclick="userRatingValue='1'">1</button>
+            <button class="rate-btn" id="rateBtn" onclick="userRatingValue='2'">2</button>
+            <button class="rate-btn" id="rateBtn" onclick="userRatingValue='3'">3</button>
+            <button class="rate-btn" id="rateBtn" onclick="userRatingValue='4'">4</button>
+            <button class="rate-btn" id="rateBtn" onclick="userRatingValue='5'">5</button>
+        </div>
+        <button onClick={()=> hanldeNavigate()} className="submitBtn" type="submit">SUBMIT</button>
+        </div>
+     </div>
+    </>
+  )
+}
+
+export default App
