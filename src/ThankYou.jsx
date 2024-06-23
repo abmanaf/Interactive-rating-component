90b96bde27043e2529e60d5af8b956ae28aeea35
Alibaba <abdulmanafaliu414@gmail.com>
2024-06-23 13:55:10 +0000

    changes made

--- src/ThankYou.jsx
+++ src/ThankYou.jsx
@@ -1,24 +1,26 @@
 import React from 'react'
 import './index.css'
+import { useLocation } from 'react-router-dom'
 
 function ThankYou() {
+  const location = useLocation()
+  const { selectedRating } = location.state || { selectedRating: 0 }
+
   return (
-    <>
-     <div className='rating-container'>
+    <div className='rating-container'>
       <div className='thank-you-image'>
-        <img src="./images/illustration-thank-you.svg" alt="icon-star" />
+        <img src="./images/illustration-thank-you.svg" alt="illustration-thank-you" />
       </div>
       <div className='selected-score'>
-        <p>You select 1 out of 5</p>
+        <p>You selected {selectedRating} out of 5</p>
       </div>
       <div className='rating-info'>
-      <h2>Thank you!</h2>
-      <p className='message' style={{marginTop: '0.5em', color: 'hsl(217, 12%, 63%)',}}>
-        We appreciate you taking the time to gice a rating. if you ever need more support, don't hesitate to get in touch!
-      </p>
+        <h2>Thank you!</h2>
+        <p className='message' style={{ marginTop: '0.5em', color: 'hsl(217, 12%, 63%)' }}>
+          We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!
+        </p>
       </div>
-     </div>
-    </>
+    </div>
   )
 }
 
