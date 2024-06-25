7538816d45363110b87c5af4674c2e3770825d00
Alibaba <abdulmanafaliu414@gmail.com>
2024-06-25 07:53:19 +0000

    parent component created

---
+++ src/Components/RatingComponent.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react'
+import '../App.css'
+import { useNavigate } from 'react-router-dom'
+
+function RatingComponent() {
+  const [count, setCount] = useState(0)
+  const [list, setList] = useState([1,2,3,4,5])
+  const navigate = useNavigate()
+
+  const handleNavigate = () => {
+    if (count > 0) {
+        navigate("/ThankYou", { state: { selectedRating: count } })
+      }
+  }
+
+  return (
+    <div className='rating-container'>
+      <img src="./images/icon-star.svg" alt="icon-star" />
+      <div className='rating-info'>
+        <h2>How did we do?</h2>
+        <p className='message' style={{ marginTop: '0.5em', color: 'hsl(217, 12%, 63%)' }}>
+          Please let us know how we did with your support request.
+          All feedback is appreciated to help us improve our offering!
+        </p>
+      </div>
+      <div>
+        <div className='rating-btn'>
+          {list.map(value => (
+            <button
+              key={value}
+              className={`rate-btn ${count === value ? 'selected' : ''}`}
+              onClick={() => setCount(value)}
+            >
+              {value}
+            </button>
+          ))}
+        </div>
+        <button
+          onClick={handleNavigate}
+          className="submitBtn"
+          type="submit"
+          disabled={count === 0}
+        >
+          SUBMIT
+        </button>
+    </div>
+    </div>
+  )
+}
+
+export default RatingComponent
---
+++ src/Components/ThankYou.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { useLocation } from 'react-router-dom'
+import "../index.css"
+
+function ThankYou() {
+  const location = useLocation()
+  const { selectedRating } = location.state || { selectedRating: 0 }
+
+  return (
+    <div className='thank-you-container'>
+      <div className='thank-you-image'>
+        <img src="./images/illustration-thank-you.svg" alt="illustration-thank-you" />
+      </div>
+      <div className='selected-score'>
+        <p>You selected {selectedRating} out of 5</p>
+      </div>
+      <div className='thank-you-message'>
+        <h2>Thank you!</h2>
+        <p className='message' style={{ marginTop: '0.5em', color: 'hsl(217, 12%, 63%)' }}>
+          We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default ThankYou
